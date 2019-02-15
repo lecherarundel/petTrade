64e139625980e7eb2190bbc9a2431467e737c6b3
Andy Chen <andychen@Andys-MacBook-Pro.local>
2019-02-16 07:27:39 +0800

    add image uploader

--- pages/post/post.js
+++ pages/post/post.js
@@ -156,12 +156,12 @@ Page({
   bindPetImageInput: function() {
     var that = this;
     wx.chooseImage({
-      count: 1,
+      count: 8,
       sourceType: ['album', 'camera'],
       success: function(res) {
         console.log(res)
         that.setData({
-          petImg: res.tempFilePaths[0]
+          petImg: res.tempFilePaths
           
         })
       },
@@ -196,5 +196,126 @@ Page({
     api.uploadData(petData)
 
   },
+
+  uploader: function () {
+
+    var that = this;
+
+    let imagesList = [];
+
+    let maxSize = 1024 * 1024;
+
+    let maxLength = 3;
+
+    let flag = true;
+
+    wx.chooseImage({
+
+      count: 6, //最多可以选择的图片总数
+
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+
+      success: function (res) {
+
+        wx.showToast({
+
+          title: '正在上传...',
+
+          icon: 'loading',
+
+          mask: true,
+
+          duration: 500
+
+        })
+
+        for (let i = 0; i < res.tempFiles.length; i++) {
+
+          if (res.tempFiles[i].size > maxSize) {
+
+            flag = false;
+
+            console.log(111)
+
+            wx.showModal({
+
+              content: '图片太大，不允许上传',
+
+              showCancel: false,
+
+              success: function (res) {
+
+                if (res.confirm) {
+
+                  console.log('用户点击确定')
+
+                }
+
+              }
+
+            });
+
+          }
+
+
+
+        }
+
+        if (res.tempFiles.length > maxLength) {
+
+          console.log('222');
+
+          wx.showModal({
+
+            content: '最多能上传' + maxLength + '张图片',
+
+            showCancel: false,
+
+            success: function (res) {
+
+              if (res.confirm) {
+
+                console.log('确定');
+
+              }
+
+            }
+
+          })
+
+        }
+
+        if (flag == true && res.tempFiles.length <= maxLength) {
+
+          that.setData({
+
+            imagesList: res.tempFilePaths
+
+          })
+
+        }
+
+       
+
+        console.log(res);
+
+      },
+
+      fail: function (res) {
+
+        console.log(res);
+
+      }
+
+    })
+
+  },
+
+
+        
+      
+       
 
 })
